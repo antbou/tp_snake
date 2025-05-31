9f22653b594dbd34945b4d2af188dbcfa06dc1aa
Anthony <bouillant.anthony@gmail.com>
2025-05-31 23:13:41 +0200

    Remove goto and restructure main loop

--- main.c
+++ main.c
@@ -131,130 +131,137 @@ int main(void) {
 		return EXIT_FAILURE;
 	}
 
-start_game:
-	bool done = false;
-	enum difficulty_level difficulty = show_start_screen(ctxt);
-	double snake_move_interval = snake_move_interval = difficulty_to_interval(difficulty);
-	if (difficulty == LEAVE) {
-		done = true;
-	}
+	bool exit_game = false;
+	while (!exit_game) {
+		gfx_clear(ctxt, EMPTY);
 
-	srand(time(NULL));
-	gfx_clear(ctxt, EMPTY);
-
-	int x_min = (BORDER_OFFSET / ZOOM) * ZOOM;
-	int y_min = (BORDER_OFFSET / ZOOM) * ZOOM;
-	int x_max = width - BORDER_OFFSET - ZOOM;
-	int y_max = height - BORDER_OFFSET - ZOOM;
-
-	// Place the borders just outside the playable area
-	int border_left = x_min - 1;
-	int border_right = x_max + ZOOM + 1;
-	int border_top = y_min - 1;
-	int border_bottom = y_max + ZOOM + 1;
-	draw_border(ctxt, border_left, border_right, border_top, border_bottom);
-
-	int playable_width = x_max - x_min;
-	int playable_height = y_max - y_min;
-	int max_snake_size = (playable_width / ZOOM) * (playable_height / ZOOM);
-	struct queue_t* queue = init_snake(x_max, y_max, ZOOM);
-	draw_snake_initial(ctxt, queue, ZOOM, SNAKE);
-	int food_counter = 1, score = 0;
-	spawn_food(ctxt, BORDER_OFFSET, ZOOM, EMPTY, FOOD);
-
-	const double frames_per_second = 60.0;
-	const double time_between_frames = 1.0 / frames_per_second * 1e6;
-
-	int last_direction, direction = right;
-	struct timespec last_food_time, last_move_time;
-	clock_gettime(CLOCK_MONOTONIC, &last_food_time);
-
-	bool first_move = true;
-	while (!done) {
-		struct timespec frame_start_time, frame_end_time, current_time;
-		clock_gettime(CLOCK_MONOTONIC, &frame_start_time);
-
-		last_direction = direction;
-		direction = get_next_direction(direction);
-
-		// Memory leaks occur in gfx_present
-		gfx_present(ctxt);
-		done = quit_signal();
-
-
-		bool has_snake_won = queue->size >= max_snake_size;
-		if (has_snake_won) {
-			printf("You win\n");
+		enum difficulty_level difficulty = show_start_screen(ctxt);
+		if (difficulty == LEAVE) {
 			break;
 		}
 
-		clock_gettime(CLOCK_MONOTONIC, &current_time);
+		double snake_move_interval = snake_move_interval = difficulty_to_interval(difficulty);
+		srand(time(NULL));
 
-		// Check if it's time to spawn food
-		double time_since_last_food = elapsed_ms(&last_food_time, &current_time);
-		bool should_spawn_food = (
-			(time_since_last_food >= FOOD_SPAWN_INTERVAL && food_counter < MAX_FOOD_COUNT)
-			|| food_counter == 0
-			);
-		if (should_spawn_food) {
-			food_counter++;
-			spawn_food(ctxt, BORDER_OFFSET, ZOOM, EMPTY, FOOD);
-			clock_gettime(CLOCK_MONOTONIC, &last_food_time);
-		}
+		// Game init
+		int x_min = (BORDER_OFFSET / ZOOM) * ZOOM;
+		int y_min = (BORDER_OFFSET / ZOOM) * ZOOM;
+		int x_max = width - BORDER_OFFSET - ZOOM;
+		int y_max = height - BORDER_OFFSET - ZOOM;
 
-		// Handles the first move to initialize the timer
-		if (first_move) {
-			clock_gettime(CLOCK_MONOTONIC, &last_move_time);
-			first_move = false;
-			continue;
-		}
+		// Place the borders just outside the playable area
+		int border_left = x_min - 1;
+		int border_right = x_max + ZOOM + 1;
+		int border_top = y_min - 1;
+		int border_bottom = y_max + ZOOM + 1;
+		draw_border(ctxt, border_left, border_right, border_top, border_bottom);
 
-		// Handles the snake movement
-		double time_since_last_move = elapsed_ms(&last_move_time, &current_time);
-		bool should_move_snake = (time_since_last_move >= snake_move_interval);
-		if (should_move_snake) {
-			struct coord_t* new_head = new_position(direction, queue->tail, ZOOM);
-			bool is_reverse_turn = (last_direction + direction == 3);
-			enum collision_type collision = get_collision_type(ctxt, new_head, ZOOM);
+		int playable_width = x_max - x_min;
+		int playable_height = y_max - y_min;
+		int max_snake_size = (playable_width / ZOOM) * (playable_height / ZOOM);
 
-			bool hit_wall_or_reverse = (collision == WALL_COLLISION || is_reverse_turn);
-			bool hit_self = (collision == SNAKE_COLLISION);
-			bool ate_food = (collision == FOOD_COLLISION);
-			if (hit_wall_or_reverse) {
-				printf("Wall collision or reverse turn detected\n");
+		struct queue_t* queue = init_snake(x_max, y_max, ZOOM);
+		draw_snake_initial(ctxt, queue, ZOOM, SNAKE);
+
+		int food_counter = 1, score = 0;
+		spawn_food(ctxt, BORDER_OFFSET, ZOOM, EMPTY, FOOD);
+
+		const double frames_per_second = 60.0;
+		const double time_between_frames = 1.0 / frames_per_second * 1e6;
+
+		int last_direction, direction = right;
+		struct timespec last_food_time, last_move_time;
+		clock_gettime(CLOCK_MONOTONIC, &last_food_time);
+
+		bool first_move = true, done = false;
+		while (!done) {
+			struct timespec frame_start_time, frame_end_time, current_time;
+			clock_gettime(CLOCK_MONOTONIC, &frame_start_time);
+
+			last_direction = direction;
+			direction = get_next_direction(direction);
+
+			// Memory leaks occur in gfx_present
+			gfx_present(ctxt);
+			done = quit_signal();
+
+
+			bool has_snake_won = queue->size >= max_snake_size;
+			if (has_snake_won) {
+				printf("You win\n");
 				break;
 			}
 
-			if (hit_self) {
-				printf("Snake self-collision detected\n");
-				break;
+			clock_gettime(CLOCK_MONOTONIC, &current_time);
+
+			// Check if it's time to spawn food
+			double time_since_last_food = elapsed_ms(&last_food_time, &current_time);
+			bool should_spawn_food = (
+				(time_since_last_food >= FOOD_SPAWN_INTERVAL && food_counter < MAX_FOOD_COUNT)
+				|| food_counter == 0
+				);
+			if (should_spawn_food) {
+				food_counter++;
+				spawn_food(ctxt, BORDER_OFFSET, ZOOM, EMPTY, FOOD);
+				clock_gettime(CLOCK_MONOTONIC, &last_food_time);
 			}
 
-			if (ate_food) {
-				score += 10;
-				printf("Food eaten!\n");
-				draw_pixel(ctxt, new_head->x, new_head->y, ZOOM, EMPTY);
-				draw_pixel(ctxt, new_head->x, new_head->y, ZOOM, SNAKE);
-				queue_enqueue(queue, new_head);
-				food_counter--;
-			} else {
-				move_snake(ctxt, queue, new_head, ZOOM, SNAKE, EMPTY);
+			// Handles the first move to initialize the timer
+			if (first_move) {
+				clock_gettime(CLOCK_MONOTONIC, &last_move_time);
+				first_move = false;
+				continue;
 			}
-			clock_gettime(CLOCK_MONOTONIC, &last_move_time);
+
+			// Handles the snake movement
+			double time_since_last_move = elapsed_ms(&last_move_time, &current_time);
+			bool should_move_snake = (time_since_last_move >= snake_move_interval);
+			if (should_move_snake) {
+				struct coord_t* new_head = new_position(direction, queue->tail, ZOOM);
+				bool is_reverse_turn = (last_direction + direction == 3);
+				enum collision_type collision = get_collision_type(ctxt, new_head, ZOOM);
+
+				bool hit_wall_or_reverse = (collision == WALL_COLLISION || is_reverse_turn);
+				bool hit_self = (collision == SNAKE_COLLISION);
+				bool ate_food = (collision == FOOD_COLLISION);
+				if (hit_wall_or_reverse) {
+					printf("Wall collision or reverse turn detected\n");
+					break;
+				}
+
+				if (hit_self) {
+					printf("Snake self-collision detected\n");
+					break;
+				}
+
+				if (ate_food) {
+					score += 10;
+					printf("Food eaten!\n");
+					draw_pixel(ctxt, new_head->x, new_head->y, ZOOM, EMPTY);
+					draw_pixel(ctxt, new_head->x, new_head->y, ZOOM, SNAKE);
+					queue_enqueue(queue, new_head);
+					food_counter--;
+				} else {
+					move_snake(ctxt, queue, new_head, ZOOM, SNAKE, EMPTY);
+				}
+				clock_gettime(CLOCK_MONOTONIC, &last_move_time);
+			}
+
+			// Handles the FPS limit
+			clock_gettime(CLOCK_MONOTONIC, &frame_end_time);
+			double frame_duration_ms = elapsed_ms(&frame_start_time, &frame_end_time);
+			double sleep_time_for_fps_limit = time_between_frames - frame_duration_ms;
+
+			if (sleep_time_for_fps_limit > 0.0) {
+				usleep((int32_t)sleep_time_for_fps_limit);
+			}
 		}
 
-		// Handles the FPS limit
-		clock_gettime(CLOCK_MONOTONIC, &frame_end_time);
-		double frame_duration_ms = elapsed_ms(&frame_start_time, &frame_end_time);
-		double sleep_time_for_fps_limit = time_between_frames - frame_duration_ms;
-
-		if (sleep_time_for_fps_limit > 0.0) {
-			usleep((int32_t)sleep_time_for_fps_limit);
+		queue_destroy(&queue);
+		bool play_again = show_end_screen(ctxt, score);
+		if (!play_again) {
+			exit_game = true;
 		}
-	}
-	queue_destroy(&queue);
-	if (!done && show_end_screen(ctxt, score)) {
-		goto start_game;
 	}
 	gfx_destroy(ctxt);
 	return EXIT_SUCCESS;
